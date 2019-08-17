5abd3f596f417f417aceead6623bed3fc55bf21d
jdc-cunningham <jcun4128@gmail.com>
2019-08-17 16:36:16 -0500

    added basic class, added add card click listener, empty validation

--- credit-card-paydown-priority-calculator/scripts/ui.js
+++ credit-card-paydown-priority-calculator/scripts/ui.js
@@ -1,8 +1,37 @@
-// class Foo extends Bar {
-//     constructor(baz) {
-//       this.baz = baz;
-//     }
-//     logBaz() {
-//       console.log(this.baz);
-//     }
-//   }
+document.addEventListener("DOMContentLoaded", function() {
+  class CardPaymentCalculator {
+    constructor() {
+      this.cardName = document.getElementById('card_name');
+      this.cardBalance = document.getElementById('card_balance');
+      this.addCardBtn = document.getElementById('add_card_btn');
+    }
+
+    getCardName() {
+      return this.cardName.value;
+    }
+
+    getCardBalance() {
+      return this.cardBalance.value;
+    }
+
+    getAddCardBtn() {
+      return this.addCardBtn;
+    }
+
+    checkEmptyFields() {
+      if (this.cardName.value === '' || this.cardBalance === '') {
+        return true;
+      }
+      return false;
+    }
+  }
+
+  const app = new CardPaymentCalculator();
+
+  app.getAddCardBtn().addEventListener('click', function() {
+    if (app.checkEmptyFields()) {
+      alert('Please fill in all fields');
+      return false;
+    }
+  });
+});
